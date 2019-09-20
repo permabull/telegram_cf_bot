3c25fe3c8558f8792c7187f7be26fb2dc5695e38
leffe <rsundh@protonmail.com>
2019-09-21 00:16:19 +0200

    new feature degen mode

--- tg_bot.sh
+++ tg_bot.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
-#TODO: Make an array of different groups to randomly flip in to confuse regulation
-
-tg_group1=test_bot
 tg_group2=Pleb_Gamble
 
 n_participants=3
-
-bet_size_procent=5
-
 startbag=10000
+old_sat_balance=0
+n_flips=-1
 
-n_flips=0
+function set_bet_size() {
+  if [ $new_sat_balance -lt $old_sat_balance ]
+  then
+    ((bet_size_procent++))
+  else
+    bet_size_procent=5
+  fi
+}
 
 while [ true ]
 do
+  telegram-cli -W -e "msg lntxbot /balance"
+  sleep 2s
 
-#Double /balance but otherwise it doesent work for some reason
-telegram-cli -W -e "msg lntxbot /balance"
-sleep 2s
-terminal_print=$(telegram-cli -W -e "msg lntxbot /balance" | grep "Balance:")
-sleep 2s
+  terminal_print=$(telegram-cli -W -e "msg lntxbot /balance" | grep "Balance:")
+  sleep 2s
 
-#Split output from terminalwindow and pick the balance at line 3
-IFS=':.' read -ra my_array <<< "$terminal_print"
-sat_balance="${my_array[2]}"
+  IFS=':.' read -ra my_array <<< "$terminal_print"
+  new_sat_balance="${my_array[2]}"
 
-#Choose what % of your bag you wanna bet
-bet_size=$(($sat_balance*$bet_size_procent/100))
-sleep 2s
+  set_bet_size
 
-#Send evil message
-telegram-cli -W -e "msg $tg_group2 optimus_cp1.0 will rek yu"
-sleep 2s
+  bet_size=$(($new_sat_balance*$bet_size_procent/100))
+  sleep 2s
 
-#Make the flip
-telegram-cli -W -e "msg $tg_group2 /coinflip $bet_size $n_participants"
-sleep 2s
+  telegram-cli -W -e "msg $tg_group2 optimus_cp0.2🤖(DGL:$bet_size_procent) will rek yu"
+  sleep 2s
 
-((n_flips++))
+  telegram-cli -W -e "msg $tg_group2 /coinflip $bet_size $n_participants"
+  sleep 2s
 
-echo "Balance --> $sat_balance satoshis"
-echo "Bet --> $bet_size satoshis"
-echo "Gains --> $(($sat_balance - $startbag)) satoshis"
-echo "Total flips --> "$n_flips
+  old_sat_balance=$new_sat_balance
 
-#Wait 31m because of regulation
-sleep 31m
+  ((n_flips++))
 
+  echo "Degenlevel --> $bet_size_procent%"
+  echo "Balance --> $new_sat_balance satoshis"
+  echo "Bet --> $bet_size satoshis"
+  echo "Gains --> $(($new_sat_balance - $startbag)) satoshis"
+  echo "Flips : $n_flips"
+
+  sleep 31m
 done
-
